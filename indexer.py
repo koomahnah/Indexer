c402f3d0426e2262f8d4f6a036468e9e95ba9189
Bartosz Szczepanek <Bartosz Szczepanek>
2024-11-23 10:54:21 +0100

    Working indexer and test cases

--- indexer.py
+++ indexer.py
@@ -6,9 +6,16 @@ import json
 
 def sha1sum(file_path):
     sha1 = hashlib.sha1()
-    with open(file_path, 'rb') as f:
-        while chunk := f.read(8192):  # Read the file in chunks (8192 bytes at a time)
-            sha1.update(chunk)
+    try:
+        with open(file_path, 'rb') as f:
+            while chunk := f.read(8192):  # Read the file in chunks (8192 bytes at a time)
+                sha1.update(chunk)
+    except PermissionError as e:
+        print(f"Skipping inaccessible file {file_path} due to missing permissions. Error: {e}")
+        return None
+    except Exception as e:
+        print(f"Skipping inaccessible file {file_path} due to: {e}")
+        return None
     return sha1.hexdigest()
 
 def index(dir):
@@ -17,6 +24,8 @@ def index(dir):
     for file_path in path.rglob('*'):  # '*' matches all files and directories
         if file_path.is_file():
             checksum = sha1sum(file_path)
+            if checksum is None:
+                continue
             if checksum in dict:
                 dict[checksum].append(str(file_path))
             else:
@@ -50,33 +59,302 @@ def deserialize_from_json(file_path):
     with path.open('r') as file:
         return json.load(file)
 
-DEBUG = False
+DEBUG = True
 
 def debug_print(*args, **kwargs):
     if DEBUG:
         print(*args, **kwargs)
 
-def compare(current, old):
+# Return arrays current_stripped, old_stripped such that
+# every SHA:filename pair present in both current and old
+# is removed.
+def strip_unchanged(current, old):
+    old_stripped = {}
+    current_stripped = {}
+    for sha in old:
+        new_sha = [x for x in old[sha] if sha not in current or x not in current[sha]]
+        if len(new_sha) != 0:
+            old_stripped[sha] = new_sha
+    for sha in current:
+        new_sha = [x for x in current[sha] if sha not in old or x not in old[sha]]
+        if len(new_sha) != 0:
+            current_stripped[sha] = new_sha
+    return current_stripped, old_stripped
+
+# Strip easy cases of content change.
+def strip_content_changes(current, old):
+    old_stripped = {}
+    current_stripped = {}
+    # Dict of path -> [old sha, new sha]
+    content_changes = {}
     current_paths_to_sha = {}
     for sha in current:
         for path in current[sha]:
             current_paths_to_sha[path] = sha
-
-    debug_print("Iterating through old index.")
     for sha in old:
-        debug_print(f"Found SHA {sha} with {len(old[sha])} filepaths")
-        if sha in current:
-            for path in old[sha]:
-                if path not in current[sha] and path not in current_paths_to_sha:
-                    print(f"File {path} was in the index, but is now missing.")
-                elif path not in current[sha] and path in current_paths_to_sha:
-                    print(f"File {path} changed its contents.")
-        else:
-            for path in old[sha]:
-                if path in current_paths_to_sha:
-                    print(f"File {path} changed its contents.")
+        for path in old[sha]:
+            if path in current_paths_to_sha and sha != current_paths_to_sha[path]:
+                content_changes[path] = [sha, current_paths_to_sha[path]]
+    
+    # Recreate indexes without pairs listed in content_changes.
+    for sha in old:
+        for path in old[sha]:
+            if path not in content_changes:
+                if sha in old_stripped:
+                    old_stripped[sha].append(path)
                 else:
-                    print(f"File {path} was in the index, but is now missing.")
+                    old_stripped[sha] = [path]
+    for sha in current:
+        for path in current[sha]:
+            if path not in content_changes:
+                if sha in current_stripped:
+                    current_stripped[sha].append(path)
+                else:
+                    current_stripped[sha] = [path]
+    return current_stripped, old_stripped, content_changes
+
+# Strip easy cases of moves/renames.
+def strip_moves(current, old):
+    old_stripped = {}
+    current_stripped = {}
+    # Dict of sha -> [[old path, new path], ...]
+    moves = {}
+    # Helper dict of sha -> [oldpath1, oldpath2...]
+    skiplist_old = {}
+    # Helper dict of sha -> [newpath1, newpath2...]
+    skiplist_current = {}
+    current_paths_to_sha = {}
+    for sha in current:
+        for path in current[sha]:
+            current_paths_to_sha[path] = sha
+    for sha in old:
+        if sha in current:
+            for i in range(min(len(old[sha]), len(current[sha]))):
+                if sha not in moves:
+                    moves[sha] = [[old[sha][i], current[sha][i]]]
+                    skiplist_old[sha] = [old[sha][i]]
+                    skiplist_current[sha] = [current[sha][i]]
+                else:
+                    moves[sha].append([old[sha][i], current[sha][i]])
+                    skiplist_old[sha].append(old[sha][i])
+                    skiplist_current[sha].append(current[sha][i])
+    
+    # Recreate indexes without pairs listed in content_changes.
+    for sha in old:
+        for path in old[sha]:
+            if sha in skiplist_old and path in skiplist_old[sha]:
+                continue
+            if sha in old_stripped:
+                old_stripped[sha].append(path)
+            else:
+                old_stripped[sha] = [path]
+    for sha in current:
+        for path in current[sha]:
+            if sha in skiplist_current and path in skiplist_current[sha]:
+                continue
+            if sha in current_stripped:
+                current_stripped[sha].append(path)
+            else:
+                current_stripped[sha] = [path]
+    return current_stripped, old_stripped, moves
+
+# Strip easy cases of removal. Assumption: content changes, moves are already removed.
+def strip_removal(current, old):
+    old_stripped = {}
+    # Dict of sha -> [old paths]
+    removals = {}
+    for sha in old:
+        if sha not in current:
+            for path in old[sha]:
+                if sha in removals:
+                    removals[sha].append(path)
+                else:
+                    removals[sha] = [path]
+    
+    # Recreate indexes without pairs listed in content_changes.
+    for sha in old:
+        for path in old[sha]:
+            if sha not in removals:
+                if sha in old_stripped:
+                    old_stripped[sha].append(path)
+                else:
+                    old_stripped[sha] = [path]
+    return current, old_stripped, removals
+
+# Strip easy cases of copies or new files. Assumption: content changes, moves, removals are already removed.
+# old_orig contains old index before any changes.
+def strip_copy_or_new(current, old, old_orig):
+    new_stripped = {}
+    old_stripped = {}
+    # Dict of sha -> [new paths]
+    new = {}
+    # Dict of sha -> [old path, [new path1, new path2...]]
+    copies = {}
+    for sha in current:
+        if sha not in old:
+            # All paths in current[sha] are new or copies
+            if sha in old_orig:
+                # It's a copy. Append to the copies dict
+                for path in current[sha]:
+                    if sha in copies:
+                        copies[sha][1].append(path)
+                    else:
+                        copies[sha] = [old_orig[sha][0], [path]]
+            else:
+                # It's a new file(s)
+                new[sha] = current[sha]
+        else:
+            '''
+            There's same SHA in both old and current. What can be the case?
+            [f1, f2, f3] -> [f1, f2, f3, f4] <- f1, f2, f3 would get stripped in earlier step
+            [f4] -> [d1/f5] <- that would get removed in "move" step
+            [f1, f2] -> [f3, f4] <- that would also get removed in "move" step
+            Cases:
+             1) same length
+             [f1] -> [f1] <- impossible because it would be stripped in "unchanged" step
+             [f1] -> [f2] <- impossible because that would get removed in "move" step
+             2) current index is longer; intersection is non-empty
+             [f1] -> [f1, f2] <- impossible because "unchanged" step
+             3) current index is longer; intersection is empty
+             [f1] -> [f2, f3, f4, f5...] <- this would get flagged as move, causing old to go empty XXX [worth checking with a test case]
+             4) old index is longer; intersection non-empty
+             [f1, f2, f3...] -> [f1] <- impossible because "unchanged" step
+             5) old index is longer; intersection empty
+             [f2, f3, f4...] -> [f1] <- this would also get flagged as move causing current to go empty XXX
+            '''
+            print(f"This is a really unexpected case. There's same SHA in both old and current index. SHA {sha}, current[sha] {current[sha]}, old[sha] {old[sha]}")
+            assert False
+
+    # Recreate indexes without pairs listed in content_changes.
+    # old index can be returned as-is, because:
+    #   1. File that was copied is removed as unchanged
+    #   2. File that's new resides only in current index
+    current_stripped = {}
+    for sha in current:
+        for path in current[sha]:
+            if sha in new and path in new[sha]:
+                continue
+            if sha in copies and path in copies[sha][1]:
+                continue
+            if sha in current_stripped:
+                current_stripped[sha].append(path)
+            else:
+                current_stripped[sha] = [path]
+    return current_stripped, old, copies, new
+
+class ChangeDescription:
+    def __init__(self):
+        self.current_stripped = {}
+        self.old_stripped = {}
+        self.content_changes = {}
+        self.moves = {}
+        self.removals = {}
+        self.copies = {}
+        self.new = {}
+    def __str__(self):
+        return (
+            f"ChangeDescription:\n"
+            f"  current_stripped: {self.current_stripped}\n"
+            f"  old_stripped: {self.old_stripped}\n"
+            f"  content_changes: {self.content_changes}\n"
+            f"  moves: {self.moves}\n"
+            f"  removals: {self.removals}\n"
+            f"  copies: {self.copies}\n"
+            f"  new: {self.new}\n"
+        )
+
+def strip(current, old, debug=False):
+    change_descr = ChangeDescription()
+    change_descr.current_stripped, change_descr.old_stripped = strip_unchanged(current, old)
+    if debug:
+        print("Stripped unchanged.", change_descr)
+    change_descr.current_stripped, change_descr.old_stripped, change_descr.content_changes = strip_content_changes(change_descr.current_stripped, change_descr.old_stripped)
+    if debug:
+        print("Stripped content changes.", change_descr)
+    change_descr.current_stripped, change_descr.old_stripped, change_descr.moves = strip_moves(change_descr.current_stripped, change_descr.old_stripped)
+    if debug:
+        print("Stripped moves.", change_descr)
+    change_descr.current_stripped, change_descr.old_stripped, change_descr.removals = strip_removal(change_descr.current_stripped, change_descr.old_stripped)
+    if debug:
+        print("Stripped removals.", change_descr)
+    change_descr.current_stripped, change_descr.old_stripped, change_descr.copies, change_descr.new = strip_copy_or_new(change_descr.current_stripped, change_descr.old_stripped, old)
+    if debug:
+        print("Stripped copies and new.", change_descr)
+    return change_descr
+
+def compare(current, old):
+    change_descr = strip(current, old)
+    for path in change_descr.content_changes:
+        print(f"File {path} changed its contents.")
+    for sha in change_descr.moves:
+        for old_path, new_path in change_descr.moves[sha]:
+            print(f"File {old_path} moved to {new_path}.")
+    for sha in change_descr.removals:
+        for path in change_descr.removals[sha]:
+            print(f"File {path} was in the index, but is now missing.")
+    for sha in change_descr.new:
+        for path in change_descr.new[sha]:
+            print(f"File {path} is new.")
+    for sha in change_descr.copies:
+        oldpath = change_descr.copies[sha][0]
+        newpaths = change_descr.copies[sha][1]
+        for path in newpaths:
+            print(f"File {oldpath} was copied to {path}.")
+
+# def compare(current, old):
+#     current_paths_to_sha = {}
+#     for sha in current:
+#         for path in current[sha]:
+#             current_paths_to_sha[path] = sha
+#     old_paths_to_sha = {}
+#     for sha in old:
+#         for path in old[sha]:
+#             old_paths_to_sha[path] = sha
+# 
+#     debug_print("Iterating through old index.")
+#     for sha in old:
+#         debug_print(f"Found old SHA {sha} with {len(old[sha])} filepaths")
+#         if sha in current:
+#             for path in old[sha]:
+#                 if path not in current[sha] and path not in current_paths_to_sha:
+#                     print(f"File {path} was in the index, but is now missing.")
+#                 elif path not in current[sha] and path in current_paths_to_sha:
+#                     print(f"File {path} changed its contents.")
+#         else:
+#             for path in old[sha]:
+#                 if path in current_paths_to_sha:
+#                     print(f"File {path} changed its contents.")
+#                 else:
+#                     print(f"File {path} was in the index, but is now missing.")
+#     
+#     for sha in current:
+#         debug_print(f"Found current SHA {sha} with {len(current[sha])} filepaths")
+#         if sha in old:
+#             if len(old[sha]) == len(current[sha]) and len(old[sha]) == 1:
+#                 # One path for SHA in both current and new. Is it the same?
+#                 if old[sha] != current[sha]:
+#                     print(f"File {old[sha]} moved to {current[sha]}.")
+#             elif old[sha].sort() == current[sha].sort():
+#                 # More paths with the same SHA, but all duplicates stay where they were.
+#                 pass
+#             else:
+#                 # Many paths with the same SHA. Current index is different than old.
+#                 # Example:
+#                 # old
+#                 #   f1.txt a39a3ee5e6
+#                 #   f2.txt a39a3ee5e6
+#                 #   f3.txt d9310ab13e
+#                 # new
+#                 #   d1/f1.txt a39a3ee5e6
+#                 #   d1/f2.txt a39a3ee5e6
+#                 #   f3.txt d9310ab13e
+#                 #   f4.txt a39a3ee5e6
+#                 print(f"Duplicated file at paths {old[sha].sort()} is now at {current[sha].sort()}")
+#         else:
+#             # This is a new SHA. Either it's a new file, or it changed its contents.
+#             for path in current[sha]:
+#                 if path not in old_paths_to_sha:
+#                     print(f"File {path} is new.")
 
 def main():
     parser = argparse.ArgumentParser(description="Indexing your files, and validating if you've lost anything.")
@@ -100,5 +378,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
--- test_indexer.py
+++ test_indexer.py
@@ -2,7 +2,178 @@ import unittest
 import subprocess
 from pathlib import Path
 import tempfile
+from indexer import strip_content_changes, strip_moves, strip_unchanged, strip_removal, strip_copy_or_new, strip, ChangeDescription
 
+class TestRemovalIsolated(unittest.TestCase):
+    def test_removal_simple(self):
+        current = {}
+        old = {'bcd': ['f3']}
+        current, old, removals = strip_removal(current, old)
+        self.assertEqual(current, {})
+        self.assertEqual(old, {})
+        self.assertEqual(removals, {'bcd': ['f3']})
+    def test_removal_multiple(self):
+        current = {'eee': ['f7']}
+        old = {'bcd': ['f3', 'f4'], 'afd': ['f5']}
+        current, old, removals = strip_removal(current, old)
+        self.assertEqual(current, {'eee': ['f7']})
+        self.assertEqual(old, {})
+        self.assertEqual(removals, {'bcd': ['f3', 'f4'], 'afd': ['f5']})
+    def test_removal_multiple_spurious(self):
+        current = {'eee': ['f7']}
+        old = {'bcd': ['f3', 'f4'], 'afd': ['f5'], 'eee': ['f8']}
+        current, old, removals = strip_removal(current, old)
+        self.assertEqual(current, {'eee': ['f7']})
+        self.assertEqual(old, {'eee': ['f8']})
+        self.assertEqual(removals, {'bcd': ['f3', 'f4'], 'afd': ['f5']})
+class TestMoveIsolated(unittest.TestCase):
+    def test_move_simple(self):
+        current = {'abc': ['f2']}
+        old = {'abc': ['f3']}
+        current, old, moves = strip_moves(current, old)
+        self.assertEqual(current, {})
+        self.assertEqual(old, {})
+        self.assertEqual(moves, {'abc': [['f3', 'f2']]})
+    def test_move_multiple(self):
+        current = {'abc': ['f3', 'f4']}
+        old = {'abc': ['f1', 'f2']}
+        current, old, moves = strip_moves(current, old)
+        self.assertEqual(current, {})
+        self.assertEqual(old, {})
+        self.assertIn('abc', moves)
+        self.assertTrue(moves == {'abc': [['f1', 'f3'], ['f2', 'f4']]} or moves == {'abc': [['f1', 'f4'], ['f2', 'f3']]})
+    def test_move_multiple_spurious(self):
+        current = {'abc': ['f3', 'f4'], 'eef': ['f8']}
+        old = {'abc': ['f1', 'f2'], 'eee': ['f4']}
+        current, old, moves = strip_moves(current, old)
+        self.assertEqual(current, {'eef': ['f8']})
+        self.assertEqual(old, {'eee': ['f4']})
+        self.assertIn('abc', moves)
+        self.assertTrue(moves == {'abc': [['f1', 'f3'], ['f2', 'f4']]} or moves == {'abc': [['f1', 'f4'], ['f2', 'f3']]})
+    def test_move_spurious(self):
+        current = {'abc': ['f2'], 'efd': ['f4', 'f1']}
+        old = {'abc': ['f3'], 'eee': ['f4']}
+        current, old, moves = strip_moves(current, old)
+        self.assertEqual(current, {'efd': ['f4', 'f1']})
+        self.assertEqual(old, {'eee': ['f4']})
+        self.assertEqual(moves, {'abc': [['f3', 'f2']]})
+    def test_move_intact(self):
+        a = {'abc': ['f1', 'f4']}
+        b = {'dfb': ['f2'], 'aaa': ['f5']}
+        c, d, f = strip_moves(a,b)
+        self.assertEqual(a, c)
+        self.assertEqual(b, d)
+        self.assertEqual(len(f), 0)
+class TestContentChangeIsolated(unittest.TestCase):
+    def test_content_change_simple(self):
+        a = {'abc': ['f2']}
+        b = {'dfb': ['f2']}
+        a, b, c = strip_content_changes(a,b)
+        self.assertEqual(len(a), 0)
+        self.assertEqual(len(b), 0)
+        self.assertIn('f2', c)
+        self.assertIn('abc', c['f2'])
+        self.assertIn('dfb', c['f2'])
+    def test_content_change_spurious(self):
+        a = {'abc': ['f2', 'f4']}
+        b = {'dfb': ['f2'], 'aaa': ['f5']}
+        a, b, c = strip_content_changes(a,b)
+        self.assertEqual(len(a), 1)
+        self.assertEqual(len(b), 1)
+        self.assertIn('abc', a)
+        self.assertIn('f4', a['abc'])
+        self.assertIn('aaa', b)
+        self.assertIn('f5', b['aaa'])
+        self.assertIn('f2', c)
+        self.assertIn('abc', c['f2'])
+        self.assertIn('dfb', c['f2'])
+    def test_content_change_intact(self):
+        a = {'abc': ['f1', 'f4']}
+        b = {'dfb': ['f2'], 'aaa': ['f5']}
+        c, d, f = strip_content_changes(a,b)
+        self.assertEqual(a, c)
+        self.assertEqual(b, d)
+        self.assertEqual(len(f), 0)
+class TestCopyOrNewIsolated(unittest.TestCase):
+    def test_new_simple(self):
+        current = {'bcd': ['f3']}
+        old = {}
+        current, old, copies, new = strip_copy_or_new(current, old, old)
+        self.assertEqual(current, {})
+        self.assertEqual(old, {})
+        self.assertEqual(new, {'bcd': ['f3']})
+        self.assertEqual(copies, {})
+    def test_copy_simple(self):
+        current = {'bcd': ['f1_copy']}
+        old = {}
+        old_prestrip = {'bcd': ['f1']}
+        current, old, copies, new = strip_copy_or_new(current, old, old_prestrip)
+        self.assertEqual(current, {})
+        self.assertEqual(old, {})
+        self.assertEqual(new, {})
+        self.assertEqual(copies, {'bcd': ['f1', ['f1_copy']]})
+    def test_copy_multiple(self):
+        current = {'bcd': ['f1_copy', 'f1_copy2', 'f1_copy3'], 'eee': ['f2_copy', 'f3_copy']}
+        old = {}
+        old_prestrip = {'bcd': ['f1'], 'eee': ['f2', 'f3']}
+        current, old, copies, new = strip_copy_or_new(current, old, old_prestrip)
+        self.assertEqual(current, {})
+        self.assertEqual(old, {})
+        self.assertEqual(new, {})
+        self.assertEqual(copies, {'bcd': ['f1', ['f1_copy', 'f1_copy2', 'f1_copy3']], 'eee': ['f2', ['f2_copy', 'f3_copy']]})
+    def test_new_multiple(self):
+        current = {'bcd': ['f3', 'd1/f3'], 'bababa': ['bababafile']}
+        old = {}
+        current, old, copies, new = strip_copy_or_new(current, old, old)
+        self.assertEqual(current, {})
+        self.assertEqual(old, {})
+        self.assertEqual(new, {'bcd': ['f3', 'd1/f3'], 'bababa': ['bababafile']})
+        self.assertEqual(copies, {})
+    def test_new_and_copy(self):
+        current = {'eee': ['f1_copy'], 'bcd': ['f3']}
+        old = {}
+        old_prestrip = {'eee': ['f1']}
+        current, old, copies, new = strip_copy_or_new(current, old, old_prestrip)
+        self.assertEqual(current, {})
+        self.assertEqual(old, {})
+        self.assertEqual(new, {'bcd': ['f3']})
+        self.assertEqual(copies, {'eee': ['f1', ['f1_copy']]})
+    def test_new_and_copy_multiple(self):
+        current = {'tdsfdfsd': ['ft', 'd1/f3'], 'bababa': ['bababafile'], 'bcd': ['f1_copy', 'f1_copy2', 'f1_copy3'], 'eee': ['f2_copy', 'f3_copy']}
+        old = {}
+        old_prestrip = {'bcd': ['f1'], 'eee': ['f2', 'f3']}
+        current, old, copies, new = strip_copy_or_new(current, old, old_prestrip)
+        self.assertEqual(current, {})
+        self.assertEqual(old, {})
+        self.assertEqual(new, {'tdsfdfsd': ['ft', 'd1/f3'], 'bababa': ['bababafile']})
+        self.assertEqual(copies, {'bcd': ['f1', ['f1_copy', 'f1_copy2', 'f1_copy3']], 'eee': ['f2', ['f2_copy', 'f3_copy']]})
+class TestFullStrip(unittest.TestCase):
+    def test_fullstrip_simple(self):
+        current = {'bcd': ['f3'], 'sha1': ['d1/f1'], 'sha3': ['f2'], 'sha4': ['f4', 'f5', 'f6']}
+        old = {'sha1': ['f1'], 'sha2': ['f2'], 'sha4': ['f4', 'f5'], 'sha5': ['f7']}
+        change_descr = strip(current, old)
+        self.assertEqual(change_descr.current_stripped, {})
+        self.assertEqual(change_descr.old_stripped, {})
+        self.assertEqual(change_descr.new, {'bcd': ['f3']})
+        self.assertEqual(change_descr.removals, {'sha5': ['f7']})
+        self.assertEqual(change_descr.copies, {'sha4': ['f4', ['f6']]})
+        self.assertEqual(change_descr.moves, {'sha1': [['f1', 'd1/f1']]})
+        self.assertEqual(change_descr.content_changes, {'f2': ['sha2', 'sha3']})
+    def test_fullstrip_move_and_delete(self):
+        current = {'bcd': ['f3'], 'sha1': ['d1/f1'], 'sha3': ['f2'], 'sha4': ['f4', 'f5', 'f6']}
+        old = {'sha1': ['f1', 'f11'], 'sha2': ['f2'], 'sha4': ['f4', 'f5'], 'sha5': ['f7']}
+        change_descr = strip(current, old)
+        self.assertEqual(change_descr.current_stripped, {})
+        self.assertEqual(change_descr.old_stripped, {})
+        self.assertEqual(change_descr.new, {'bcd': ['f3']})
+#        self.assertIn('sha5', change_descr.removals, {'sha5': ['f7'], 'sha1': ['f11']})
+        self.assertIn('sha5', change_descr.removals)
+        self.assertIn('f7', change_descr.removals['sha5'])
+        self.assertIn('sha1', change_descr.removals)
+        self.assertIn('f11', change_descr.removals['sha1'])
+        self.assertEqual(change_descr.copies, {'sha4': ['f4', ['f6']]})
+        self.assertEqual(change_descr.moves, {'sha1': [['f1', 'd1/f1']]})
+        self.assertEqual(change_descr.content_changes, {'f2': ['sha2', 'sha3']})
 class TestContentChange(unittest.TestCase):
     def setUp(self):
         self.temp_dir = tempfile.TemporaryDirectory()
